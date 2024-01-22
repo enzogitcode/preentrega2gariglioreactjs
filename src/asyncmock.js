5a7259187bdce53d0f6f194cc0ce2f8913949259
enzogit <enzo_91_10@hotmail.com>
2024-01-22 19:41:36 -0300

    ultimas modificaciones

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,10 +1,11 @@
 const productos = [
     { id: "1", nombre: "Pavlova", precio: 12000, img: "../imgs/pavlova.webp", idCat: "Tortas" },
     { id: "2", nombre: "Torta Oreo", precio: 14000, img: "../imgs/tortaoreo.jpg", idCat: "Tortas" },
-    { id: "3", nombre: "Medialunas rellenas", precio: 200, img: "", idCat: "Laminados" },
+    { id: "3", nombre: "Medialunas rellenas", precio: 200, img: "../imgs/medialunasrellenas.jpg", idCat: "Laminados" },
     { id: "4", nombre: "Medialunas", precio: 150, img: "../imgs/medialunas.webp", idCat: "Laminados" },
-    { id: "5", nombre: "Rogel", precio: 1100, img: "../imgs/rogel.jpg", idCat: "Tortas" },]
-
+    { id: "5", nombre: "Rogel", precio: 1100, img: "../imgs/rogel.jpg", idCat: "Tortas" },
+    { id: "6", nombre: "Tiramisú", precio: 10000, img: "../imgs/tiramisu.jpg", idCat: "Tortas"},
+]
 export const getProductos = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -4,8 +4,8 @@ import './ItemDetail.css';
 const ItemDetail = ({id, nombre, precio, img}) => {
   return (
     <div className='contenedorItem'>
-        <h2>Nombre: {nombre} </h2>
-        <h3>Precio: $ {precio} </h3>
+        <h2 className='itemDetailNombre'>Nombre: {nombre} </h2>
+        <h3 className='itemDetailPrecio'>Precio: $ {precio} </h3>
         <p>ID: {id} </p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto ea eum in consequatur nesciunt dolores nam, fugiat eligendi ipsa esse quod voluptatem accusamus facere natus! Numquam expedita ut repellendus inventore!</p>
         <img src={img} alt={nombre} />
