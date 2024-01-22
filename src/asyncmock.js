4b650d4606d09fdc5ce5a97bd1c12b8363762ed7
enzogit <enzo_91_10@hotmail.com>
2024-01-22 18:25:35 -0300

    cambios css

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const productos = [
-    { id: "1", nombre: "Pavlova", precio: 500, img: "../imgs/pavlova.webp", idCat: "Tortas" },
-    { id: "2", nombre: "Torta Oreo", precio: 180, img: "../imgs/tortaoreo", idCat: "Tortas" },
+    { id: "1", nombre: "Pavlova", precio: 12000, img: "../imgs/pavlova.webp", idCat: "Tortas" },
+    { id: "2", nombre: "Torta Oreo", precio: 14000, img: "../imgs/tortaoreo.jpg", idCat: "Tortas" },
     { id: "3", nombre: "Medialunas rellenas", precio: 200, img: "", idCat: "Laminados" },
-    { id: "4", nombre: "Medialunas", precio: 1100, img: "../imgs/medialunas.webp", idCat: "Laminados" },
+    { id: "4", nombre: "Medialunas", precio: 150, img: "../imgs/medialunas.webp", idCat: "Laminados" },
     { id: "5", nombre: "Rogel", precio: 1100, img: "../imgs/rogel.jpg", idCat: "Tortas" },]
 
 export const getProductos = () => {
