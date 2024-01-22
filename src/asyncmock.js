0e865fe9f7906a0fd5740614d8b8d372196e378e
enzogit <enzo_91_10@hotmail.com>
2024-01-22 19:56:07 -0300

    precios

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const productos = [
     { id: "1", nombre: "Pavlova", precio: 12000, img: "../imgs/pavlova.webp", idCat: "Tortas" },
     { id: "2", nombre: "Torta Oreo", precio: 14000, img: "../imgs/tortaoreo.jpg", idCat: "Tortas" },
-    { id: "3", nombre: "Medialunas rellenas", precio: 200, img: "../imgs/medialunasrellenas.jpg", idCat: "Laminados" },
-    { id: "4", nombre: "Medialunas", precio: 150, img: "../imgs/medialunas.webp", idCat: "Laminados" },
+    { id: "3", nombre: "Medialunas rellenas", precio: 500, img: "../imgs/medialunasrellenas.jpg", idCat: "Laminados" },
+    { id: "4", nombre: "Medialunas", precio: 450, img: "../imgs/medialunas.webp", idCat: "Laminados" },
     { id: "5", nombre: "Rogel", precio: 1100, img: "../imgs/rogel.jpg", idCat: "Tortas" },
     { id: "6", nombre: "Tiramisú", precio: 10000, img: "../imgs/tiramisu.jpg", idCat: "Tortas"},
 ]
